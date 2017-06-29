ce19d86fd9e01b524a0a9c98e411c1bea56bf834
Liang <liang@LiangdeMacBook-Pro.local>
2017-06-29 21:08:48 +0800

    ——>苹果内购模块 修正：隐私协议链接异常

--- YFBFriend/Class/Base/Model/YFBApplePayManager.m
+++ YFBFriend/Class/Base/Model/YFBApplePayManager.m
@@ -9,6 +9,12 @@
 #import "YFBApplePayManager.h"
 #import <StoreKit/StoreKit.h>
 
+NSString *const kYFBOpenVipGlodkeyName = @"OPEN_VIP_1";
+
+@interface YFBApplePayManager () <SKProductsRequestDelegate,SKPaymentTransactionObserver>
+
+@end
+
 @implementation YFBApplePayManager
 
 + (instancetype)manager {
@@ -20,6 +26,48 @@
     return _applePayManager;
 }
 
+- (void)getProductionInfos {
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    if ([SKPaymentQueue canMakePayments]) {
+        SKProductsRequest *productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithArray:@[@"YPB_VIP_1Month",@"YPB_VIP_3Month"]]];
+        productsRequest.delegate = self;
+        [productsRequest start];
+    } else {
+        [[YFBHudManager manager] showHudWithText:@"请设置允许应用内购买"];
+    }
+}
+
+#pragma mark - SKProductsRequestDelegate
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response {
+//    _priceArray = [[NSMutableArray alloc] init];
+//    NSLog(@"-----------收到产品反馈信息--------------");
+//    NSArray *myProduct = response.products;
+//    NSLog(@"-------------myProduct-----------------%lu",(unsigned long)myProduct.count);
+//    NSLog(@"无效产品Product ID:%@",response.invalidProductIdentifiers);
+//    NSLog(@"产品付费数量: %d", (int)[myProduct count]);
+//    // populate UI
+//    for(SKProduct *product in myProduct){
+//        NSLog(@"--------------------");
+//        NSLog(@"product info");
+//        NSLog(@"SKProduct 描述信息%@", [product description]);
+//        NSLog(@"产品标题 %@" , product.localizedTitle);
+//        NSLog(@"产品描述信息: %@" , product.localizedDescription);
+//        NSLog(@"价格: %@" , product.price);
+//        NSLog(@"Product id: %@" , product.productIdentifier);
+//        [_priceArray addObject:product.price];
+//    }
+//    
+//    [self setPriceInfoWithArray:_priceArray];
+
+}
+
+
+#pragma mark - SKPaymentTransactionObserver
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions {
+    
+}
 
 
 @end
